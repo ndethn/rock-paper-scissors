eb2fb5debce80c770556aecb8bfddccb430a28c0
Ethan <uhhhhhethan@gmail.com>
2022-10-27 10:22:02 -0700

    Create playRound function and declare variables playerSelection and computerSelection, and declare conditions through if else statements

--- app.js
+++ app.js
@@ -22,11 +22,13 @@ function getComputerChoice() {
 write a function for a single round
 - takes two parameters: playerSelection and computerSelection
 - return a string declaring the winner: "You Lose! Paper beats Rock"
-- make playerSelection function parameter case-insensitive
+- make playerSelection function parameter case-insensitive -> convert the users input
+into all lowercase: toLowerCase()
 
 - create function playRound containing parameters playerSelection and computerSelection
 - compare the two selections to declare a winner
 - return a string declaring the winner
+- return the result of the function call
 
 - use an IF statement
     - if playerSelection === computerSelection
@@ -50,6 +52,35 @@ write a function for a single round
     - else if playerSelection === 'scissors' && computerSelection === 'paper'
     - then return you win
 
-
+- declare playerSelection by using prompt();
+- declare computerSelection equal to the function getComputerChoice()
 
 */
+
+// testing the playRound function
+function playRound(playerSelection, computerSelection){
+    // tie condition
+    if (playerSelection === computerSelection) {
+        return 'It\'s a tie!';
+    } else if (playerSelection === 'rock' && computerSelection === 'paper'){
+        return 'You lose! Paper beats Rock';
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors'){
+        return 'You win! Rock beats Scissors';
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors'){
+        return 'You lose! Scissors beats Paper';
+    } else if (playerSelection === 'paper' && computerSelection === 'rock'){
+        return 'You win! Paper beats rock';
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock'){
+        return 'You lose! Rock beats scissors';
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper'){
+        return 'You win! Scissors beats paper';
+    } else {
+        return 'try again';
+    }
+}
+
+// declare playerSelection and computerSelection
+const playerSelection = prompt('Rock, paper, scissors, GO!').toLowerCase();
+const computerSelection = getComputerChoice();
+console.log(`Computer chose: ${computerSelection}`);
+console.log(playRound(playerSelection, computerSelection));
