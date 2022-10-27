d72a05cde35e26fad1d9156cc3d87c712e7f5585
Ethan <uhhhhhethan@gmail.com>
2022-10-27 09:47:00 -0700

    Write pseudocode for the playRound function

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 /***** 
-
 pseudocode -> rock paper scissors
 
 begin with a function getComputerChoice()
@@ -7,8 +6,6 @@ begin with a function getComputerChoice()
 - declare as an array containing these choices
 - will randomly return either 'rock', 'paper', 'scissors'
 - use console to verify
-
-
 *****/
 
 // declare choices array
