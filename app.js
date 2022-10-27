a5cf9e9d2a3290af1121f5a3063c13fc49d2b670
Ethan <uhhhhhethan@gmail.com>
2022-10-27 09:08:58 -0700

    Edit pseudocode in JS file and link external JS in HTML file q

---
+++ app.js
@@ -0,0 +1,12 @@
+/***** 
+
+pseudocode -> rock paper scissors
+
+begin with a function getComputerChoice()
+- random choice between rock, paper, scissors
+- declare as an array containing these choices
+- will randomly return either 'rock', 'paper', 'scissors'
+- use console to verify
+
+
+*****/
