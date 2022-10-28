9a838cae37aee65c688eea877c865db8b4bd7228
Ethan <uhhhhhethan@gmail.com>
2022-10-28 12:54:46 -0700

    Appriopriately run five rounds of the game within the game function

--- app.js
+++ app.js
@@ -58,22 +58,22 @@ into all lowercase: toLowerCase()
 */
 
 // testing the playRound function
-function playRound(playerSelection, computerSelection){
+function playRound(playerSelection, computerSelection) {
     // tie condition
     if (playerSelection === computerSelection) {
         return 'It\'s a tie!';
     } // all other conditions
-    else if (playerSelection === 'rock' && computerSelection === 'paper'){
+    else if (playerSelection === 'rock' && computerSelection === 'paper') {
         return 'You lose! Paper beats Rock';
-    } else if (playerSelection === 'rock' && computerSelection === 'scissors'){
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
         return 'You win! Rock beats Scissors';
-    } else if (playerSelection === 'paper' && computerSelection === 'scissors'){
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
         return 'You lose! Scissors beats Paper';
-    } else if (playerSelection === 'paper' && computerSelection === 'rock'){
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
         return 'You win! Paper beats rock';
-    } else if (playerSelection === 'scissors' && computerSelection === 'rock'){
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
         return 'You lose! Rock beats scissors';
-    } else if (playerSelection === 'scissors' && computerSelection === 'paper'){
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
         return 'You win! Scissors beats paper';
     } // consider typos
     else {
@@ -83,11 +83,11 @@ function playRound(playerSelection, computerSelection){
 
 // declare playerSelection and computerSelection
 // note: need a condition for cancel button or exit -> reading null error for toLowerCase()
-const playerSelection = prompt('Rock, Paper, Scissors, GO!').toLowerCase();
-const computerSelection = getComputerChoice();
-console.log(`Computer chose: ${computerSelection}`);
-console.log(`You chose: ${playerSelection}`);
-console.log(playRound(playerSelection, computerSelection));
+// const playerSelection = prompt('Rock, Paper, Scissors, GO!').toLowerCase();
+// const computerSelection = getComputerChoice();
+// console.log(`Computer chose: ${computerSelection}`);
+// console.log(`You chose: ${playerSelection}`);
+// console.log(playRound(playerSelection, computerSelection));
 
 
 /* declare new function called game()
@@ -106,24 +106,37 @@ console.log(playRound(playerSelection, computerSelection));
 */
 
 // declare function game()
-function game() {
-    // for loop to run 5 rounds
-    // initialize rounds to 0
-    let numRounds = 0;
-    for (let i = 0; i < 5; i++){
-        // code block to call the playRound function
-        // increment each round by 1 to total 5
-        numRounds += 1;
-        console.log(numRounds); // prints 1,2,3,4,5
-        if (numRounds === 5){
-            console.log('Game done!');
-        } else {
-            console.log('Next round');
-        }
-    }
-    // playRound(playerSelection,computerSelection);
-    // console.log(playRound(playerSelection,computerSelection));
-    return numRounds;
-}
-console.log(game()); // prints 5
+// function game() {
+//     // for loop to run 5 rounds
+//     // initialize rounds to 0
+//     let numRounds = 0;
+//     for (let i = 0; i < 5; i++){
+//         // code block to call the playRound function
+//         // increment each round by 1 to total 5
+//         numRounds += 1;
+//         console.log(numRounds); // prints 1,2,3,4,5
+//         if (numRounds === 5){
+//             console.log('Game done!');
+//         } else {
+//             console.log('Next round');
+//         }
+//     }
+//     // playRound(playerSelection,computerSelection);
+//     // console.log(playRound(playerSelection,computerSelection));
+//     return numRounds;
+// }
+// console.log(game()); // prints 5
 // console.log(playRound(playerSelection, computerSelection));
+
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = prompt('Rock, Paper, Scissors, GO!').toLowerCase();
+        const computerSelection = getComputerChoice();
+        console.log(`Round ${i + 1}`);
+        console.log(`Computer chose: ${computerSelection}`);
+        console.log(`You chose: ${playerSelection}`);
+        console.log(playRound(playerSelection, computerSelection));
+    }
+}
+game();
